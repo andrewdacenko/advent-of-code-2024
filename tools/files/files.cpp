b52efe23cd211dc2b97207f5a8d68aec58200d3a
Andrew Dacenko <a.s.dvornik@gmail.com>
2024-12-08 17:03:53 +0000

    [files] fix line parsing
    Summary:
    
    Test Plan:

--- tools/files/files.cpp
+++ tools/files/files.cpp
@@ -1,9 +1,11 @@
 #include "files.hpp"
 #include <fstream>
 #include <ranges>
+#include <string>
+#include <vector>
 
 namespace files {
-std::string getContent(const std::string& filename) {
+std::string getContent(const std::string &filename) {
   std::ifstream file(filename);
   if (!file.is_open()) {
     throw std::ios_base::failure("Could not open file " + filename);
@@ -12,14 +14,18 @@ std::string getContent(const std::string& filename) {
   return {std::istreambuf_iterator(file), {}};
 }
 
-std::vector<std::string> getLines(const std::string& filename) {
+std::vector<std::string> getLines(const std::string &filename) {
   std::ifstream file(filename);
 
   if (!file.is_open()) {
     throw std::ios_base::failure("Could not open file " + filename);
   }
 
-  return {std::ranges::istream_view<std::string>(file) |
-          std::ranges::to<std::vector<std::string>>()};
-}
+  std::string line;
+  std::vector<std::string> lines;
+  while (std::getline(file, line)) {
+    lines.push_back(line);
+  }
+  return {lines};
+};
 } // namespace files
