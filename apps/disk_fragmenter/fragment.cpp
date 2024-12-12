89ce1786d7afe27fc5f6462ce23ca1564af4df7e
Andrew Dacenko <a.s.dvornik@gmail.com>
2024-12-12 02:07:42 +0000

    day 9 part 2

--- apps/disk_fragmenter/fragment.cpp
+++ apps/disk_fragmenter/fragment.cpp
@@ -11,6 +11,10 @@ void prettyPrint(const std::vector<Block> &blocks) {
 }
 
 Num checksum(const std::string &data, bool second) {
+  if (second) {
+    return checksumFiles(data);
+  }
+
   std::vector<Block> buffer;
   for (int i = 0; i < data.size(); i++) {
     int num = data[i] - '0';
@@ -33,5 +37,57 @@ Num checksum(const std::string &data, bool second) {
   }
 
   return chechsum;
+}
+
+Num checksumFiles(const std::string &data) {
+  std::vector<File> disk;
+  int addr = 0;
+  for (int i = 0; i < data.size(); i += 2) {
+    int size = data[i] - '0';
+    int empty = i + 1 < data.size() ? data[i + 1] - '0' : 0;
+    disk.push_back({addr, Num(i / 2), size, empty});
+    addr += size + empty;
+  }
+
+  auto calc = [](int addr, Num id, int n) {
+    Num sum = 0;
+    for (int i = 0; i < n; i++) {
+      sum += (addr + i) * id;
+    }
+    return sum;
+  };
+
+  Num checksum = 0;
+  for (auto start = disk.begin(), end = disk.end() - 1; end != start;) {
+    auto destination = start;
+    auto file = end;
+    for (;;) {
+      if (destination >= file - 1) {
+        break;
+      }
+
+      if (destination->empty >= file->size) {
+        break;
+      }
+
+      destination++;
+    }
+
+    if (destination == file || destination->empty < file->size) {
+      checksum += calc(file->addr, file->id, file->size);
+      end--;
+      continue;
+    }
+
+    destination->empty -= file->size;
+    auto fileChecksum =
+        calc(destination->addr + destination->size + destination->filled,
+             file->id, file->size);
+    destination->filled += file->size;
+    checksum += fileChecksum;
+    end--;
+  }
+
+  return checksum;
 }
 } // namespace fragment
--- apps/disk_fragmenter/fragment.hpp
+++ apps/disk_fragmenter/fragment.hpp
@@ -1,4 +1,6 @@
 #include <string>
+#include <utility>
+#include <vector>
 
 namespace fragment {
 using Num = unsigned long;
@@ -6,5 +8,13 @@ struct Block {
   Num id;
   bool allocated;
 };
+struct File {
+  int addr;
+  Num id;
+  int size;
+  int empty;
+  int filled;
+};
 Num checksum(const std::string &data, bool second);
+Num checksumFiles(const std::string &data);
 } // namespace fragment
